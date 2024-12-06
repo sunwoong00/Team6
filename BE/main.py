452671724eaafc22c08d382b15d783ed155bb77c
sunwoong <sunwoong89@g.skku.edu>
2024-12-06 22:46:50 +0900

    FastAPI test

--- BE/main.py
+++ BE/main.py
@@ -1,1 +1,14 @@
-test
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+#async def root():
+def root():
+    return {"message": "World"}
+
+
+@app.get("/home")
+def home():
+    return {"message": "home"}
