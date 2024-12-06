2d823f199b802f60097b3bf29153e456ab057de0
sunwoong <sunwoong89@g.skku.edu>
2024-12-07 00:22:48 +0900

    Login

--- BE/main.py
+++ BE/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 import firebase_admin
 from firebase_admin import credentials, db
 
@@ -9,13 +9,15 @@ app = FastAPI()
 # Firebase 초기화
 cred = credentials.Certificate("team6-8924e-firebase-adminsdk-vdshl-543124bd12.json")  # Firebase JSON 키 파일 경로
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://Team6.firebaseio.com/'  # Firebase Realtime Database URL
+    'databaseURL': 'https://team6-8924e-default-rtdb.firebaseio.com/'  # Firebase Realtime Database URL
 })
 
 # Pydantic 모델
 class LoginData(BaseModel):
     username: str
     password: str
+    id: EmailStr
+    user_protector: bool
 
 # /login 엔드포인트
 @app.post("/login")
@@ -25,7 +27,14 @@ async def login(data: LoginData):
         ref = db.reference('users')  # 'users'라는 노드에 데이터 저장
         ref.push({
             'username': data.username,
-            'password': data.password  # 실제 서비스에서는 비밀번호를 암호화하세요!
+            'id': data.id,
+            'password': data.password,
+            'user_protector': data.user_protector,
+            'gps': None,
+            'stt_data': None,
+            'slm_llm': None,
+            'slm_data': None,
+            'llm_data': None
         })
         return {"message": "User saved successfully"}
     except Exception as e:
