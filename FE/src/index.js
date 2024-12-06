4ca2fdf5bf479d43e2302b09a377f37d3bf19a94
qianyi-yu <yuqianyi1409@gmail.com>
2024-12-07 02:49:56 +0900

    feat: add InputBox & Button component

--- FE/src/index.js
+++ FE/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Popup from '../src/component/Popup'
 import ToggleButton from '../src/component/ToggleButton';
+import Button from '../src/component/Button';
+import InputBox from '../src/component/InputBox'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
